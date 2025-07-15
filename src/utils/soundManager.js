9ad6b17b1e27e43053621df0f67736276900f7f6
Abi <kabishake191@gmail.com>
2025-07-15 14:16:17 +0530

    Initial commit

--- src/utils/soundManager.js
+++ src/utils/soundManager.js
@@ -2,7 +2,7 @@
 import { Howl } from 'howler';
 
 const sounds = {
-  tick: new Howl({ src: ['/sounds/tick3.mp3'], volume: 0.5 }),
+  tick: new Howl({ src: ['/sounds/tickf2.mp3'], volume: 0.5 }),
   start: new Howl({ src: ['/sounds/start.mp3'], volume: 0.6 }),
   stop: new Howl({ src: ['/sounds/stop.mp3'], volume: 0.6 }),
   lap: new Howl({ src: ['/sounds/lap.mp3'], volume: 0.5 }),
